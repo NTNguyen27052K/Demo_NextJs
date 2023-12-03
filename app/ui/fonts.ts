e6c38ce6347ff6bc82024cd7db94c0fba3e4919b
NTNguyen27052K <ntnguyen275@gmail.com>
2023-12-03 20:18:36 +0700

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Changa } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const changa = Changa({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
